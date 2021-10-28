b2b9eac6a42189f0beb87f0e287e6ab69b0de368
saratbhargava <sarat2895@gmail.com>
2021-10-28 02:34:07 -0700

    clean up package building with setup tools

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Analyze CT Scan Images',
+    name='img_recognition',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    url="https://github.com/saratbhargava/img_recognition",
+    version='0.0.1',
+    description='Implement DL based Image recognition systems from scatch',
     author='Sarat Chinni',
+    author_email='sarat.chinni@outlook.com',
     license='MIT',
 )
