80a89caa6af3778a86c95f575ca3f5cc8cd73546
saratbhargava <sarat2895@gmail.com>
2021-10-27 20:45:52 -0700

    Added src folder and more folders from datascience template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze CT Scan Images',
+    author='Sarat Chinni',
+    license='MIT',
+)
