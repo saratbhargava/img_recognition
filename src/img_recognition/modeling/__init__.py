8b47115837157493c0f799511fd409a457892304
saratbhargava <sarat2895@gmail.com>
2021-10-30 10:56:50 -0700

    add more models to build

--- src/img_recognition/modeling/__init__.py
+++ src/img_recognition/modeling/__init__.py
@@ -1,6 +1,14 @@
+from typing import Any
+
 from .example_model import ResNet18
+from img_recognition.modeling.resnet_model import ResNet
 
 
-def build_model(cfg):
-    model = ResNet18(cfg.MODEL.NUM_CLASSES)
+def build_model(cfg) -> Any:
+    if cfg.MODEL.NAME == 'resnet18':
+        model = ResNet18(cfg.MODEL.NUM_CLASSES)
+    elif cfg.MODEL.NAME == 'resnet_n':
+        model = ResNet(
+            n = cfg.MODEL.NUM_RES_BLOCKS,
+            num_classes = cfg.MODEL.NUM_CLASSES)
     return model
